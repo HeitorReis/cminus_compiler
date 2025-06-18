95def24f3ea339ac6dae11c6e6c2abb9052553d0
Heitor Reis <43591059+HeitorReis@users.noreply.github.com>
2025-06-18 13:49:54 -0300

    Fix error output to stderr

--- main.c
+++ main.c
@@ -30,7 +30,7 @@ int main(int argc, char **argv) {
     printf("Analisando o arquivo: %s\n", argv[1]);
     syntaxTree = parse();
     if (parseResult != 0 || erro_lexico != 0) {
-        printf(stderr, "Erro na análise sintática.\n");
+        fprintf(stderr, "Erro na análise sintática.\n");
         fclose(yyin);
         return EXIT_FAILURE;
     }
